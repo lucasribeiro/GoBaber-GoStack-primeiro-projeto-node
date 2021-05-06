ee70a9a92a5b5ae3f8db68a5bc2012d61f99fe4f
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-05-05 21:17:33 -0300

    Comit Inicial

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { startOfHour } from 'date-fns';
+
+interface Request {
+    provider: string;
+    date: Date;
+} 
+
+class CreateAppointmentService {
+    
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({provider, date}: Request): Appointment {
+
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSamedate = this.appointmentsRepository.findByDate(appointmentDate);
+        
+        if (findAppointmentInSamedate){
+            throw Error('Este horário já existe agendamento.');
+        }
+
+        const appointment = this.appointmentsRepository.create({provider, date: appointmentDate});
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
